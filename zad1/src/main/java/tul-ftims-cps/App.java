6aa39d3af2414567608d89615d62a0e52a08ee67
Gracjan Grala <gracjan.grala.tel@gmail.com>
2019-03-26 13:53:50 +0100

    Init project

---
+++ zad1/src/main/java/tul-ftims-cps/App.java
@@ -0,0 +1,13 @@
+package tul-ftims-cps;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
